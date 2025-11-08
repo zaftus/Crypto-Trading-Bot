f8c8e2988bd5a4c1a64f8bdd58c667c5bc4dddb3
Ankith <160985511+zaftus@users.noreply.github.com>
2025-11-07 23:58:51 -0500

    Create logger.py

---
+++ python/logger.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(
+    filename='bot.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+def log_info(message):
+    logging.info(message)
+
+def log_error(message):
+    logging.error(message)
