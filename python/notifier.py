a5d4e46ec8cabf3113ac7c5a64b9fdfa493ea7a6
Ankith <160985511+zaftus@users.noreply.github.com>
2025-11-08 00:00:26 -0500

    Create notifier.py

---
+++ python/notifier.py
@@ -0,0 +1,7 @@
+import requests
+from config.api_keys import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
+    requests.post(url, data=payload)
