0fd133b8bda47df411216e2c4d95a78f465a6e62
Ankith <160985511+zaftus@users.noreply.github.com>
2025-11-07 23:57:33 -0500

    Create api_keys.py

---
+++ config/api_keys.py
@@ -0,0 +1,6 @@
+# Store your API keys here (DO NOT push to public GitHub!)
+BINANCE_API_KEY = "your_binance_api_key"
+BINANCE_SECRET_KEY = "your_binance_secret_key"
+COINGECKO_API_KEY = "your_coingecko_api_key"
+TELEGRAM_BOT_TOKEN = "your_telegram_token"
+TELEGRAM_CHAT_ID = "your_chat_id"
